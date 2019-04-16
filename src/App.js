7e38a8b7819466ad7d9918d589e220d049a2d84b
root <root@localhost.localdomain>
2019-04-16 19:17:57 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+           	Hey DIALLO 
+		How are the people we are going to check 
           </p>
           <a
             className="App-link"
